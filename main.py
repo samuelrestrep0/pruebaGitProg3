09439f96a662417570d8ab76b0062d09a87242d9
Samuel Restrepo <restredual@gmail.com>
2024-09-09 10:53:18 -0500

    implementacion resta

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 def suma(x,y):
-        return x+y
+    return x+y
 
-respuesta=suma(5,7)
-print(respuesta)
+def resta (x,y):
+    return x-y        
+
+respuestaSuma=suma(5,7)
+respuestaResta=(5,7)
+print("el resultado de la suma es "+respuestaSuma+" Y el de la resta es "+respuestaResta)
+
