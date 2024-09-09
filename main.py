347c1cb1f3245f4fcc5beb1613e000d29d265d61
Samuel Restrepo <restredual@gmail.com>
2024-09-09 10:54:21 -0500

    correccion de la resta

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ def resta (x,y):
     return x-y        
 
 respuestaSuma=suma(5,7)
-respuestaResta=(5,7)
+respuestaResta=resta(5,7)
 print("el resultado de la suma es "+respuestaSuma+" Y el de la resta es "+respuestaResta)
 
