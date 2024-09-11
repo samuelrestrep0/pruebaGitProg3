8bc821e602de427c4937a569da390fe50b7037f6
edissonRamirez <edisson.ramirez29529@ucaldas.edu.co>
2024-09-11 08:40:36 -0500

    Implementé la division

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def resta (x,y):
     return x-y        
 
-respuesta=resta(5,7)
-print( "el resultado es "+respuesta)
+def division (x,y):
+    return x/y
 
+respuesta=resta(5,7)
+print( "el resultado es "+str(respuesta))
+resdivi = division(10,2)
+print(resdivi)
